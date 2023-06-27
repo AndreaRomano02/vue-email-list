d2bf6ec21327bb9f2269e97f883e345ec6be3162
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-27 14:05:34 +0200

    Add 10 random mails from AJAX

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-console.log('VUE OK', Vue);
-
 //# Creo la pagina VEU
 const { createApp } = Vue;
 
 const myApp = createApp({
   data() {
-    return {};
+    return {
+      emails: [],
+    };
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+        this.emails.push(res.data.response);
+      });
+    }
   },
 });
 
